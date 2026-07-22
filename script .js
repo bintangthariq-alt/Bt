559f00e372e2e23d0ad1d24c110bd7bc3d314f29
bintangthariq-alt <bintangselalubersinar047@gmail.com>
2026-07-22 22:15:05 +0700

    Update script .js

--- script .js
+++ script .js
@@ -1,17 +1,1 @@
-// Tombol Subway Surfers
-document.getElementById('playBtn').addEventListener('click', () => {
-    window.open('https://poki.com/id/g/subway-surfers', '_blank');
-});
 
-// Semua tombol kartu game
-document.querySelectorAll('.card-play').forEach((btn, index) => {
-    const linkGame = [
-        'https://poki.com/id/g/hill-climb-racing',
-        'https://poki.com/id/g/basketball-stars',
-        'https://poki.com/id/g/block-puzzle',
-        'https://poki.com/id/g/soccer-skills'
-    ];
-    btn.addEventListener('click', () => {
-        window.open(linkGame[index], '_blank');
-    });
-});
